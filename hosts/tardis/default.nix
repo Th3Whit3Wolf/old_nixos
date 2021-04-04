541f80b60b0f31d7f94692497a0087651b969919
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2021-04-04 12:35:34 +0100

    Added tlp settings

--- hosts/tardis/default.nix
+++ hosts/tardis/default.nix
@@ -64,6 +64,7 @@
       zsh.enable = true;
     };
     services = {
+      psd.enable = true;
       ssh.enable = true;
       # Needed occasionally to help the parental units with PC problems
       # teamviewer.enable = true;
@@ -79,7 +80,45 @@
   services = {
     openssh.startWhenNeeded = true;
     chrony.enable = true;
-    tlp.enable = true;
+    tlp = {
+      enable = true;
+      settings = {
+	"SOUND_POWER_SAVE_ON_AC" = 0;
+	"SOUND_POWER_SAVE_ON_BAT" = 1;
+	"SOUND_POWER_SAVE_CONTROLLER" = "Y";
+	"BAY_POWEROFF_ON_AC" = 0;
+	"BAY_POWEROFF_ON_BAT" = 1;
+	"DISK_APM_LEVEL_ON_AC" = "254 254";
+	"DISK_APM_LEVEL_ON_BAT" = "128 128";
+	"DISK_IOSCHED" = "none none";
+	"SATA_LINKPWR_ON_AC" = "med_power_with_dipm max_performance";
+	"SATA_LINKPWR_ON_BAT" = "min_power";
+	"MAX_LOST_WORK_SECS_ON_AC" = 15;
+	"MAX_LOST_WORK_SECS_ON_BAT" = 60;
+	"NMI_WATCHDOG" = 0;
+	"WIFI_PWR_ON_AC" = "off";
+	"WIFI_PWR_ON_BAT" = "on";
+	"WOL_DISABLE" = "Y";
+	"CPU_SCALING_GOVERNOR_ON_AC" = "powersave";
+	"CPU_SCALING_GOVERNOR_ON_BAT" = "powersave";
+	"CPU_MIN_PERF_ON_AC" = 0;
+	"CPU_MAX_PERF_ON_AC" = 100;
+	"CPU_MIN_PERF_ON_BAT" = 0;
+	"CPU_MAX_PERF_ON_BAT" = 50;
+	"CPU_BOOST_ON_AC" = 1;
+	"CPU_BOOST_ON_BAT" = 1;
+	"SCHED_POWERSAVE_ON_AC" = 0;
+	"SCHED_POWERSAVE_ON_BAT" = 1;
+	"ENERGY_PERF_POLICY_ON_AC" = "performance";
+	"ENERGY_PERF_POLICY_ON_BAT" = "power";
+	"RESTORE_DEVICE_STATE_ON_STARTUP" = 0;
+	"RUNTIME_PM_ON_AC" = "on";
+	"RUNTIME_PM_ON_BAT" = "auto";
+	"PCIE_ASPM_ON_AC" = "default";
+	"PCIE_ASPM_ON_BAT" = "powersupersave";
+	"USB_AUTOSUSPEND" = 1;
+      };
+    };
     sshguard.enable = true;
     irqbalance.enable = true;
     udev = {
@@ -119,5 +158,6 @@
     [
       #gnome-podcasts
       lollypop
+      git-crypt
     ];
 }
