489d86a6029f35508d642e0c3e8db06c64a02bbd
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2021-04-04 10:06:29 +0100

    Auto logout on TTY

--- hosts/home.nix
+++ hosts/home.nix
@@ -37,9 +37,16 @@ with lib; {
       "/persist/etc/ssh_host_ed25519_key.pub";
     "NetworkManager/system-connections".source =
       "/persist/etc/NetworkManager/system-connections";
+    "profile.d/shell-timeout.sh".text ='' "TMOUT="\$(( 60*30 ))";
+      [ -z "\$DISPLAY" ] && export TMOUT;
+        case \$( /usr/bin/tty ) in
+	  /dev/tty[0-9]*) export TMOUT;;
+        esac
+    '';
   };
   systemd.tmpfiles.rules = [
     "L /var/lib/NetworkManager/secret_key - - - - /persist/var/lib/NetworkManager/secret_key"
     "L /var/lib/NetworkManager/timestamps - - - - /persist/var/lib/NetworkManager/timestamps"
+    "L /var/lib/sshguard/blacklist.db - - - - /persist/var/lib/sshguard/blacklist.db"
   ];
 }
