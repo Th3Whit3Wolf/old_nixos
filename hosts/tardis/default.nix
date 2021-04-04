7574b1785437323297d54d9f3eb71356c0b5b46e
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2021-04-04 10:06:43 +0100

    Better laptop perf and battery

--- hosts/tardis/default.nix
+++ hosts/tardis/default.nix
@@ -50,7 +50,7 @@
       };
       vcs = { git.enable = true; };
     };
-    editors = {
+    editor = {
       default = "nvim";
       emacs.enable = false;
       nvim.enable = true;
@@ -72,14 +72,35 @@
   };
 
   ## Local config
-  programs.ssh.startAgent = true;
+  programs = {
+    ssh.startAgent = true;
+    nm-applet.enable = true;
+  };
   services = {
     openssh.startWhenNeeded = true;
     chrony.enable = true;
+    tlp.enable = true;
+    sshguard.enable = true;
+    irqbalance.enable = true;
+    udev = {
+      extraRules = ''
+        # set scheduler for NVMe
+        ACTION=="add|change", KERNEL=="nvme[0-9]*", ATTR{queue/scheduler}="none"
+        # set scheduler for SSD and eMMC
+        ACTION=="add|change", KERNEL=="sd[a-z]|mmcblk[0-9]*", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="mq-deadline"
+        # set scheduler for rotating disks
+        ACTION=="add|change", KERNEL=="sd[a-z]", ATTR{queue/rotational}=="1", ATTR{queue/scheduler}="bfq"
+        # Save Power
+	ACTION=="add", SUBSYSTEM=="scsi_host", KERNEL=="host*", ATTR{link_power_management_policy}="med_power_with_dipm"
+      '';
+    };   
   };
 
   networking = {
-    networkmanager.enable = true;
+    networkmanager = {
+      enable = true;
+      wifi.powersave = true;
+    };
     useDHCP = false;
     timeServers = [
       "0.uk.pool.ntp.org"
@@ -92,8 +113,9 @@
       "3.nixos.pool.ntp.org"
     ];
   };
-
-  environment.systemPackages = with pkgs;
+  
+  environment.systemPackages = [ pkgs.irqbalance pkgs.dbus-broker ];
+  user.packages = with pkgs;
     [
       #gnome-podcasts
       lollypop
