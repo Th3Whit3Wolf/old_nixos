df01fdd17a5caf52236b95165a319b8a8e6dfe7c
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2021-04-11 11:40:42 +0100

    Remove theme

--- hosts/tardis/default.nix
+++ hosts/tardis/default.nix
@@ -69,7 +69,7 @@
       # Needed occasionally to help the parental units with PC problems
       # teamviewer.enable = true;
     };
-    theme.active = "alucard";
+    # theme.active = "alucard";
   };
 
   ## Local config
