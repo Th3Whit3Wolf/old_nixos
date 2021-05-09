49dd557b1ac3b099001d5c677d1ce9ee88ea7d09
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2021-05-09 15:16:52 +0100

    Add root password

--- hosts/tardis/users/default.nix
+++ hosts/tardis/users/default.nix
@@ -1,5 +1,14 @@
+{ lib, ... }:
+let
+  inherit (builtins) toFile readFile;
+  inherit (lib) fileContents mkForce;
+
+in
 {
   imports = [
     ./doc.nix
   ];
+
+  users.users.root.initialHashedPassword = fileContents ../secrets/root;
+
 }
