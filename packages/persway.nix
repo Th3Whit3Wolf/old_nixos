aceef2119a5d1c658f6f3bd0706887141562d900
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2021-05-28 22:56:44 +0100

    Add persway

---
+++ packages/persway.nix
@@ -0,0 +1,25 @@
+{ lib, fetchFromGitLab, rustPlatform }:
+
+rustPlatform.buildRustPackage rec {
+  pname = "persway";
+  version = "0.4.1";
+
+  src = fetchFromGitHub {
+    owner = "johnae";
+    repo = "persway";
+    rev = "3908055d2334253d357bb90d9872213dc08fbd20";
+    sha256 = "sha256-97qJd3o8nJt8IX5tyGWtAmJsIv5Gcw1xoBFwxAqk7I8=";
+  };
+
+  cargoSha256 = "sha256-c/30fqLOw1WvDRNgH+Su0i0kNzWPZ+qZJ6tHGS+UWjM=";
+  meta = {
+    license = pkgs.stdenv.lib.licenses.mit;
+    maintainers = [
+      {
+        email = "john@insane.se";
+        github = "johnae";
+        name = "John Axel Eriksson";
+      }
+    ];
+  };
+}
