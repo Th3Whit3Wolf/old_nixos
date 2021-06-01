8f0eb521458d45e48658f322da9e03dbc5c61c4f
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2021-06-01 16:02:08 +0100

    Added crucial settings

--- hosts/tardis/default.nix
+++ hosts/tardis/default.nix
@@ -2,8 +2,6 @@
 let
   inherit (builtins) toFile readFile;
   inherit (lib) fileContents mkForce;
-
-  name = "Just the doctor";
 in
 {
   imports = [
@@ -11,11 +9,20 @@ in
     ./hardware-configuration.nix
   ] ++ suites.mobile;
 
+  # Use the latest kernel
+  boot = {
+    kernelPackages = pkgs.linuxPackages_5_10;
+    loader = {
+      efi.canTouchEfiVariables = true;
+      systemd-boot.configurationLimit = 10;
+      systemd-boot.enable = true;
+    };
+  };
+
 
   time.timeZone = "Europe/London";
 
-
-  nix.maxJobs = lib.mkDefault 8;
+  nix.maxJobs = 8;
   environment.etc = {
     nixos.source = "/persist/etc/nixos";
     "NetworkManager/system-connections".source = "/persist/etc/NetworkManager/system-connections";
