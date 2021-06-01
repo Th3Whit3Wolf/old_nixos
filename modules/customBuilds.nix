b9e1bc7bdaaa822f5f52e73897089d815c6e9151
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2021-06-01 13:33:49 +0100

    Remove old devos lib

--- modules/customBuilds.nix
+++ modules/customBuilds.nix
@@ -1,13 +1,14 @@
-{ lib, self, devlib, config, modules, channel, ... }:
+{ lib, self, diggaLib, config, modules, channel, ... }:
 let
   mkBuild = buildModule:
-    channel.input.lib.nixosSystem (devlib.mergeAny config.lib.builderArgs {
+    # TODO: get specialArgs as a module argument and drop builderArgs usage
+    channel.input.lib.nixosSystem (diggaLib.mergeAny config.lib.builderArgs {
       modules = [ buildModule ];
     });
 in
 {
   system.build = {
-    iso = (mkBuild (devlib.modules.isoConfig {
+    iso = (mkBuild (diggaLib.modules.isoConfig {
       inherit self;
       inherit (self) inputs;
       fullHostConfig = config;
