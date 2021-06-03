ab8bf8d1c58643eca09dad2a884cbbd22cc5b70b
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2021-06-03 11:59:38 +0100

    Add wayland-nixpkg binary-cache

---
+++ profiles/cachix/wayland.nix
@@ -0,0 +1,10 @@
+{
+  nix = {
+    binaryCachePublicKeys = [
+      "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+    ];
+    binaryCachePublicKeys = [
+      "https://nixpkgs-wayland.cachix.org"
+    ];
+  };
+}
