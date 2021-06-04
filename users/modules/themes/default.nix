0a01e56346fb81951c62b975b6a1cf02ea68703a
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2021-06-04 21:38:31 +0100

    Define theme wallpaper

--- users/modules/themes/default.nix
+++ users/modules/themes/default.nix
@@ -2,18 +2,28 @@
 
 let
 
-  inherit (lib) mkDefault mkIf mkOption types;
+  inherit (lib) mkDefault mkIf mkOption mkOpt
+    types;
 
 in
 {
   imports = [ ./spaceDark ];
   options = {
-    home.theme = mkOption {
-      type = types.str;
-      example = "Space Dark";
-      description = ''
-        The theme name.
-      '';
+    home.theme = with types; {
+      name = mkOption {
+        type = with types; nullOr str;
+        example = "Space Dark";
+        description = ''
+          The theme name.
+        '';
+      };
+      wallpaper = mkOption {
+        type = with types; nullOr path;
+        example = "../Wallpaper/1.jpg";
+        description = ''
+          The theme wallpaper.
+        '';
+      };
     };
   };
 }
