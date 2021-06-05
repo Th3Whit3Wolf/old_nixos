a4701bac23ee25f761a98544dcdfc6b42a56b60f
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2021-06-05 22:58:10 +0100

    Add opengl for tardis

--- hosts/tardis/default.nix
+++ hosts/tardis/default.nix
@@ -48,4 +48,15 @@ in
     #  };
     #};
   };
+  hardware = {
+    opengl = {
+      enable = true;
+      extraPackages = with pkgs; [
+        vaapiVdpau
+        libvdpau-va-gl
+      ];
+      driSupport = true;
+      driSupport32Bit = true;
+    };
+  };
 }
