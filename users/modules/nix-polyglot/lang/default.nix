5a30f74ab5f99f93578da19808bcf5461900c709
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2021-06-12 13:49:13 +0100

    More robust lang default.nix

--- users/modules/nix-polyglot/lang/default.nix
+++ users/modules/nix-polyglot/lang/default.nix
@@ -1,3 +1,10 @@
-{ 
-  imports = [ ./rust ];
+{ pkgs, lib, ... }:
+let
+  folder = ./.;
+  toImport = name: value: folder + ("/" + name);
+  filterCaches = key: value: value == "directory";
+  imports = lib.mapAttrsToList toImport (lib.filterAttrs filterCaches (builtins.readDir folder));
+in
+{
+  inherit imports;
 }
