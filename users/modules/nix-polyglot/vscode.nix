9e689fe43c299e7355461c34edcaafe8f5db499b
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2021-06-13 15:24:57 +0100

    Add more vscode extensions

--- users/modules/nix-polyglot/vscode.nix
+++ users/modules/nix-polyglot/vscode.nix
@@ -7,44 +7,43 @@ let
     cfg = config.nix-polyglot.vscode;
     polyglot = config.nix-polyglot;
 
-    jsonFormat = pkgs.formats.json { };
     vscodePname = cfg.package.pname;
     aliases = if  (vscodePname == "vscodium") then {code = "codium";} else {};
     defaultExt = with pkgs.vscode-extensions; [
-    #pkief.material-icon-theme
+    pkief.material-icon-theme
     #ms-azuretools.vscode-docker
     #ms-kubernetes-tools.vscode-kubernetes-tools
-    #ms-vscode-remote.remote-ssh
-    #ms-vscode-remote.remote-ssh-edit
+    ms-vscode-remote.remote-ssh
+    ms-vscode-remote.remote-ssh-edit
     redhat.vscode-yaml
-    #coenraads.bracket-pair-colorizer-2
-    #oderwat.indent-rainbow
-    #mechatroner.rainbow-csv
-    #mikestead.dotenv
-    #roscop.activefileinstatusbar
-    #yzhang.markdown-all-in-one
-    #shd101wyy.markdown-preview-enhanced
-    #adpyke.codesnap
-    #pflannery.vscode-versionlens
-    #dendron.dendron
-    #tamasfe.even-better-toml
-    #christian-kohler.path-intellisense
-    #aaron-bond.better-comments
-    #KnisterPeter.vscode-github
-    #hardikmodha.create-tests
-    #formulahendry.code-runner
-    #kruemelkatze.vscode-dashboard
-    #alefragnani.project-manager
-    #Gruntfuggly.todo-tree
-    #wwm.better-align
-    #Rubymaniac.vscode-paste-and-indent
-    #Tyriar.sort-lines
-    #softwaredotcom.swdc-vscode
-    #ryu1kn.edit-with-shell
-    #spikespaz.vscode-smoothtype
-    #stkb.rewrap
-    #zxh404.vscode-proto3
-    #skellock.just
+    coenraads.bracket-pair-colorizer-2
+    oderwat.indent-rainbow
+    mechatroner.rainbow-csv
+    mikestead.dotenv
+    roscop.activefileinstatusbar
+    yzhang.markdown-all-in-one
+    shd101wyy.markdown-preview-enhanced
+    adpyke.codesnap
+    pflannery.vscode-versionlens
+    dendron.dendron
+    tamasfe.even-better-toml
+    christian-kohler.path-intellisense
+    aaron-bond.better-comments
+    KnisterPeter.vscode-github
+    hardikmodha.create-tests
+    formulahendry.code-runner
+    kruemelkatze.vscode-dashboard
+    alefragnani.project-manager
+    gruntfuggly.todo-tree
+    wwm.better-align
+    rubymaniac.vscode-paste-and-indent
+    tyriar.sort-lines
+    softwaredotcom.swdc-vscode
+    ryu1kn.edit-with-shell
+    spikespaz.vscode-smoothtype
+    stkb.rewrap
+    zxh404.vscode-proto3
+    skellock.just
   ];
     
 in {
