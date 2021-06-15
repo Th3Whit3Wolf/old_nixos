e6253dbfeec8e4da3604603dbb0d6fb1152c90c6
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2021-06-15 10:42:25 +0100

    Make erase your darling more robust

--- users/profiles/eraseYourDarlings/default.nix
+++ users/profiles/eraseYourDarlings/default.nix
@@ -2,42 +2,94 @@
 
 let
   inherit (config.home) homeDirectory username;
+
+  npg = config.nix-polyglot;
+  xdg = config.xdg;
+  relToHome = xdgDir: builtins.substring  (builtins.stringLength homeDirectory)  (builtins.stringLength xdgDir) xdgDir;
+
+  conf = if xdg.enable then  (relToHome xdg.configHome) else ".config";
+  cache = if xdg.enable then  (relToHome xdg.cacheHome) else ".cache";
+  data = if xdg.enable then  (relToHome xdg.dataHome) else ".local/share";
+  desktop = if xdg.userDirs.enable then  (relToHome xdg.userDirs.desktop) else "Desktop";
+  documents = if xdg.userDirs.enable then  (relToHome xdg.userDirs.documents) else "Documents";
+  download = if xdg.userDirs.enable then  (relToHome xdg.userDirs.download) else "Downloads";
+  music = if xdg.userDirs.enable then  (relToHome xdg.userDirs.music) else "Music";
+  pictures = if xdg.userDirs.enable then  (relToHome xdg.userDirs.pictures) else "Pictures";
+  publicShare = if xdg.userDirs.enable then  (relToHome xdg.userDirs.publicShare) else "Public";
+  templates = if xdg.userDirs.enable then  (relToHome xdg.userDirs.templates) else "Templates";
+  videos = if xdg.userDirs.enable then  (relToHome xdg.userDirs.videos) else "Videos";
+  extraUserDirs = if xdg.userDirs.enable then (lib.attrsets.mapAttrsToList (name: value: relToHome "${value}") xdg.userDirs.extraConfig) else [];
+
+  vscodePname = npg.vscode.package.pname;
+  vscodeConfigDir = {
+    "vscode" = "Code";
+    "vscode-insiders" = "Code - Insiders";
+    "vscodium" = "VSCodium";
+  }.${vscodePname};
+  vscodeExtensionDir = {
+    "vscode" = ".vscode";
+    "vscode-insiders" = ".vscode-insiders";
+    "vscodium" = ".vscode-oss";
+  }.${vscodePname};
+
 in
 {
-  home.persistence."/persist/home/doc" = {
+  home.persistence."/persist/${homeDirectory}" = {
     directories = [
-      ".config/pipewire/media-session.d"
+      "${cache}/gstreamer-1.0"
+      "${cache}/lollypop"
+      "${cache}/nix-index"
+      "${conf}/pipewire/media-session.d"
       ".gnupg"
-      ".cache/starship"
-      ".cache/gstreamer-1.0"
-      ".cache/lollypop"
-      ".cache/nix-index"
-      ".cache/zsh/"
-      ".config/VSCodium/"
-      ".local/share/cargo"
-      ".local/share/direnv"
-      ".local/share/gnupg"
-      ".local/share/icons"
-      ".local/share/keyrings"
-      ".local/share/lollypop"
-      ".local/share/nu"
-      ".local/share/nvim"
-      ".local/share/rustup"
-      ".local/share/themes"
-      ".local/share/Trash"
-      ".local/share/zoxide"
-      ".local/share/zsh"
-      ".mozilla/firefox"
+      "${data}/direnv"
+      "${data}/gnupg"
+      "${data}/icons"
+      "${data}/keyrings"
+      "${data}/lollypop"
+      "${data}/nu"
+      "${data}/themes"
+      "${data}/Trash"
+      "${desktop}"
+      "${documents}"
+      "${download}"
+      "${pictures}"
+      "${publicShare}"
+      "${templates}"
+      "${videos}"
       ".ssh"
-      ".vscode-oss"
-      "Code"
-      "Desk"
-      "Docs"
-      "Downs"
-      "Pics"
-      "Tunes"
-      "Vids"
-      "Gits"
+    ] 
+    ++ extraUserDirs
+    ++ lib.optionals (npg.vscode.enable) [
+      "${conf}/pipewire/media-session.d"
+      "${conf}/${vscodeConfigDir}/Service Worker"
+      "${conf}/.pki"
+      "${vscodeExtensionDir}"
+    ]
+    ++ lib.optionals (npg.neovim.enable) [
+      "${data}/nvim"
+    ]
+    ++ lib.optionals (npg.lang.rust.enable) [
+      "${data}/cargo"
+      "${data}/rustup"
+    ]
+    ++ lib.optionals (config.programs.firefox.enable) [
+      ".mozilla/firefox"
+    ]
+    ++ lib.optionals (config.programs.ZSH.enable) [
+      "${cache}/zsh/"
+      "${data}/zsh"
+    ]
+    ++ lib.optionals (config.programs.ZSH.integrations.zoxide.enable) [
+      "${data}/zoxide"
+    ]
+    ++ lib.optionals (config.programs.ZSH.integrations.starship.enable) [
+      "${cache}/starship"
+    ];
+
+
+    files = [] ++ lib.optionals (npg.vscode.enable) [
+      "${conf}/${vscodeConfigDir}/QuotaManager"
+      "${conf}/${vscodeConfigDir}/QuotaManager-journal"
     ];
 
     allowOther = true;
