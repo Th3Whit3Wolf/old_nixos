41248aa889ef235dbf5fecd40ed17654bd37a080
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2021-06-29 21:18:45 +0100

    Update flakes

--- flake.nix
+++ flake.nix
@@ -1,81 +1,69 @@
 {
   description = "A highly structured configuration database.";
 
-  inputs =
-    {
-      nixos.url = "nixpkgs/nixos-21.05";
-      nixos-hardware.url = "github:nixos/nixos-hardware";
-      latest.url = "nixpkgs";
-      digga.url = "github:divnix/digga/develop";
+  inputs = {
+    nixos.url = "nixpkgs/nixos-21.05";
+    nixos-hardware.url = "github:nixos/nixos-hardware";
+    latest.url = "nixpkgs";
+    digga.url = "github:divnix/digga/develop";
 
-      ci-agent = {
-        url = "github:hercules-ci/hercules-ci-agent";
-        inputs = {
-          nix-darwin.follows = "darwin";
-          nixos-20_09.follows = "nixos";
-          nixos-unstable.follows = "latest";
-        };
+    ci-agent = {
+      url = "github:hercules-ci/hercules-ci-agent";
+      inputs = {
+        nix-darwin.follows = "darwin";
+        nixos-20_09.follows = "nixos";
+        nixos-unstable.follows = "latest";
       };
-
-      darwin = {
-        url = "github:LnL7/nix-darwin";
-        inputs.nixpkgs.follows = "latest";
-      };
-
-      home = {
-        url = "github:nix-community/home-manager";
-        inputs.nixpkgs.follows = "nixos";
-      };
-
-      impermanence = {
-        url = "github:nix-community/impermanence";
-        flake = false;
-      };
-
-      agenix = {
-        url = "github:ryantm/agenix";
-        inputs.nixpkgs.follows = "latest";
-      };
-
-      naersk = {
-        url = "github:nmattia/naersk";
-        inputs.nixpkgs.follows = "latest";
-      };
-
-      rust = {
-        url = "github:oxalica/rust-overlay";
-        inputs.nixpkgs.follows = "latest";
-      };
-
-      pkgs = {
-        url = "path:./pkgs";
-        inputs.nixpkgs.follows = "nixos";
-      };
-
-      #neovim-overlay = {
-      #  url = "github:nix-community/neovim-nightly-overlay";
-      #  inputs.nixpkgs.follows = "latest";
-      #};
-
-      #wayland-overlay = {
-      #  url = "github:colemickens/nixpkgs-wayland";
-      #  inputs.nixpkgs.follows = "nixos";
-      #};
     };
 
-  outputs =
-    { self
-    , pkgs
-    , digga
-    , nixos
-    , ci-agent
-    , home
-    , nixos-hardware
-    , nur
-    , agenix
-    , rust
-    , ...
-    }@inputs:
+    darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "latest";
+    };
+
+    home = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixos";
+    };
+
+    impermanence = {
+      url = "github:nix-community/impermanence";
+      flake = false;
+    };
+
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs.nixpkgs.follows = "latest";
+    };
+
+    naersk = {
+      url = "github:nmattia/naersk";
+      inputs.nixpkgs.follows = "latest";
+    };
+
+    rust = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "latest";
+    };
+
+    pkgs = {
+      url = "path:./pkgs";
+      inputs.nixpkgs.follows = "nixos";
+    };
+
+    #neovim-overlay = {
+    #  url = "github:nix-community/neovim-nightly-overlay";
+    #  inputs.nixpkgs.follows = "latest";
+    #};
+
+    #wayland-overlay = {
+    #  url = "github:colemickens/nixpkgs-wayland";
+    #  inputs.nixpkgs.follows = "nixos";
+    #};
+  };
+
+  outputs = { self, pkgs, digga, nixos, ci-agent, home, nixos-hardware, nur
+    , agenix, rust, ... }@inputs:
     digga.lib.mkFlake {
       inherit self inputs;
 
@@ -99,9 +87,7 @@
 
       sharedOverlays = [
         (final: prev: {
-          lib = prev.lib.extend (lfinal: lprev: {
-            our = self.lib;
-          });
+          lib = prev.lib.extend (lfinal: lprev: { our = self.lib; });
         })
       ];
 
@@ -132,52 +118,30 @@
           };
           suites = with profiles; rec {
             base = [ cachix core ];
-            psd = [
-              desktop.browser.psd
-            ];
-            tardis = [
-              base
-              laptop
-              psd
-              eraseYourDarlings
-            ];
+            psd = [ desktop.browser.psd ];
+            tardis = [ base laptop psd eraseYourDarlings ];
           };
         };
       };
 
       home = {
         modules = ./users/modules/module-list.nix;
-        externalModules = [
-          "${inputs.impermanence}/home-manager.nix"
-        ];
+        externalModules = [ "${inputs.impermanence}/home-manager.nix" ];
         importables = rec {
           profiles = digga.lib.importers.rakeLeaves ./users/profiles;
           suites = with profiles; rec {
             base = [ core direnv git xdg ssh ];
-            zsh = [shell.ZSH];
+            zsh = [ shell.ZSH ];
             firefox = [ desktop.browser.firefox ];
-            psd = [
-              desktop.browser.psd
-            ];
+            psd = [ desktop.browser.psd ];
             sway = [ desktop.wm.sway ];
             waybar = [ desktop.bar.waybar ];
-            tardis = [
-              base
-              dev
-              psd
-              firefox
-              sway
-              waybar
-              eraseYourDarlings
-              zsh
-            ];
+            tardis = [ base dev psd firefox sway waybar eraseYourDarlings zsh ];
           };
         };
       };
 
-      devshell.externalModules = { pkgs, ... }: {
-        packages = [ pkgs.agenix ];
-      };
+      devshell.externalModules = { pkgs, ... }: { packages = [ pkgs.agenix ]; };
 
       homeConfigurations =
         digga.lib.mkHomeConfigurations self.nixosConfigurations;
--- pkgs/flake.nix
+++ pkgs/flake.nix
@@ -18,52 +18,54 @@
       url = "github:supercomputra/SF-Mono-Font";
       flake = false;
     };
+
+    # Vim Plugins
+    vim-cargo-make = {
+      url = "github:nastevens/vim-cargo-make";
+      flake = false;
+    };
+
+    vim-duckscript = {
+      url = "github:nastevens/vim-duckscript";
+      flake = false;
+    };
+
+    ron-vim = {
+      url = "github:ron-rs/ron.vim";
+      flake = false;
+    };
   };
 
   outputs = { self, nixpkgs, ... }: {
-    overlay = final: prev: {
-      inherit (self) srcs;
-    };
+    overlay = final: prev: { inherit (self) srcs; };
 
-    srcs =
-      let
-        inherit (nixpkgs) lib;
+    srcs = let
+      inherit (nixpkgs) lib;
 
-        mkVersion = name: input:
-          let
-            inputs = (builtins.fromJSON
-              (builtins.readFile ./flake.lock)).nodes;
+      mkVersion = name: input:
+        let
+          inputs = (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes;
 
-            ref =
-              if lib.hasAttrByPath [ name "original" "ref" ] inputs
-              then inputs.${name}.original.ref
-              else "";
+          ref = if lib.hasAttrByPath [ name "original" "ref" ] inputs then
+            inputs.${name}.original.ref
+          else
+            "";
 
-            version =
-              let version' = builtins.match
-                "[[:alpha:]]*[-._]?([0-9]+(\.[0-9]+)*)+"
-                ref;
-              in
-              if lib.isList version'
-              then lib.head version'
-              else if input ? lastModifiedDate && input ? shortRev
-              then "${lib.substring 0 8 input.lastModifiedDate}_${input.shortRev}"
-              else null;
-          in
-          version;
-      in
-      lib.mapAttrs
-        (pname: input:
-          let
-            version = mkVersion pname input;
-          in
-          input // { inherit pname; }
-          // lib.optionalAttrs (! isNull version)
-            {
-              inherit version;
-            }
-        )
-        (lib.filterAttrs (n: _: n != "nixpkgs")
-          self.inputs);
+          version = let
+            version' =
+              builtins.match "[[:alpha:]]*[-._]?([0-9]+(.[0-9]+)*)+" ref;
+          in if lib.isList version' then
+            lib.head version'
+          else if input ? lastModifiedDate && input ? shortRev then
+            "${lib.substring 0 8 input.lastModifiedDate}_${input.shortRev}"
+          else
+            null;
+        in version;
+    in lib.mapAttrs (pname: input:
+      let version = mkVersion pname input;
+      in input // {
+        inherit pname;
+      } // lib.optionalAttrs (!isNull version) { inherit version; })
+    (lib.filterAttrs (n: _: n != "nixpkgs") self.inputs);
   };
 }
