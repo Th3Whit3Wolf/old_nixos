e37d45dd9e7179d02da49c624144e644126866fa
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2021-07-10 15:08:44 +0100

    Increase maximum ram usage for tmpfs

--- hosts/tardis/hardware-configuration.nix
+++ hosts/tardis/hardware-configuration.nix
@@ -58,7 +58,7 @@
     "/" = {
       device = "tmpfs";
       fsType = "tmpfs";
-      options = [ "defaults" "size=2G" "mode=755" "nodev" "nosuid" "noexec" ];
+      options = [ "defaults" "size=8G" "mode=755" "nodev" "nosuid" "noexec" ];
     };
 
     "/nix" = {
