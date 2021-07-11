53f8517e7006101c8996ccfe3b85bd33a8b6a57b
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2021-07-11 09:22:16 +0100

    Make tmp mount point for nixos
    This allows nixpkgs that need to run executables in order to build.

--- hosts/tardis/hardware-configuration.nix
+++ hosts/tardis/hardware-configuration.nix
@@ -58,8 +58,14 @@
     "/" = {
       device = "tmpfs";
       fsType = "tmpfs";
+      options = [ "defaults" "size=3G" "mode=755" "nodev" "nosuid" "noexec" ]; 
+    };
+
+    "/tmp" = {
+      device = "tmpfs";
+      fsType = "tmpfs";
       # Note: insufficient size may cause nixos-rebuild to fail (nixos builds on /tmp)
-      options = [ "defaults" "size=8G" "mode=755" "nodev" "nosuid" "noexec" ];
+      options = [ "defaults" "size=8G" "mode=755" "nodev" "nosuid"  ]; 
     };
 
     "/nix" = {
@@ -76,6 +82,7 @@
         "nodev"
         "nosuid"
       ];
+      neededForBoot = true;
     };
 
     "/persist" = {
