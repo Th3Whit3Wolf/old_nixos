d47730de2dd4b2835896ccf905dd00c43807a606
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2021-07-13 23:37:10 +0100

    Moving into pkgs

--- overlays/vimPlugins.nix
+++
@@ -1,9 +0,0 @@
-final: prev: {
-  vimPlugins = with final.vimUtils;
-    prev.vimPlugins // {
-      vim-cargo-make = buildVimPluginFrom2Nix {
-        inherit (prev.sources.vimPlugins-vim-cargo-make) version src;
-        pname = removePrefix "vimPlugins-" prev.sources.vimPlugins-vim-cargo-make.pname;
-      };
-    };
-}
