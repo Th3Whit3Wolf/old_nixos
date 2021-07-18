b0f9f1e994a55ebd11553c25a5e24d7b94d29065
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2021-07-18 09:24:35 +0100

    Format files

--- users/profiles/desktop/wm/river/default.nix
+++ users/profiles/desktop/wm/river/default.nix
@@ -8,9 +8,9 @@ let
     "${pkgs.swaylock-effects}/bin/swaylock --screenshots --clock --indicator --indicator-radius 100 --indicator-thickness 7 --effect-blur 7x5 --effect-vignette 0.5:0.5 --effect-pixelate 3 --ring-color 5d4d7a --grace 2 --fade-in 0.7";
 in
 {
-    home.configFile = {
-      "river/init" = {
-	text = '' 
+  home.configFile = {
+    "river/init" = {
+      text = ''
 #!/bin/sh
 mod="Mod4"
 riverctl map normal $mod Return spawn alacritty
@@ -19,7 +19,7 @@ riverctl map normal $mod W spawn firefox
 riverctl map normal $mod Q close
 # Mod+E to exit river
 riverctl map normal $mod E exit
-	'';
-      };
+  '';
     };
+  };
 }
