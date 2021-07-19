74eb9d7b8ac3bfa6e292e2c5b4c9b64bf30039ab
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2021-07-19 22:48:25 +0100

    [Nix-Polyglot] Added nix support

---
+++ users/modules/nix-polyglot/lang/nix/vscode.nix
@@ -0,0 +1,36 @@
+{ config, options, lib, pkgs, ... }:
+with lib;
+with builtins;
+let
+  currLang = baseNameOf (builtins.toString ./.);
+  polyglot = config.nix-polyglot;
+  cfg = polyglot.lang.${currLang};
+  enabled = (elem currLang polyglot.langs || elem "all" polyglot.langs) && polyglot.vscode.enable;
+  jsonFormat = pkgs.formats.json { };
+
+  langExtensions = with pkgs.vscode-extensions; [
+    jnoortheen.nix-ide
+    arrterian.nix-env-selector
+  ];
+
+in
+{
+  options.nix-polyglot.lang.${currLang}.vscode = {
+    extensions = mkOption {
+      type = types.listOf types.package;
+      default = langExtensions;
+      example = literalExample "[ pkgs.vscode-extensions.matklad.rust-analyzer ]";
+      description = ''
+        The extensions Visual Studio Code should use with for programming in ${currLang}.
+        These will override but not delete manually installed ones.
+      '';
+    };
+    settings = mkOption {
+      type = jsonFormat.type;
+      default = (toJSON { });
+      description = ''
+        User settings for vscode related to ${currLang}.
+      '';
+    };
+  };
+}
