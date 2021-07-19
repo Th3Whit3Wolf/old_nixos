2004370e4f527fc981e95944ff0e567d0b0d56ed
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2021-07-19 18:44:39 +0100

    Remove useless comments

--- users/modules/nix-polyglot/lang/default.nix
+++ users/modules/nix-polyglot/lang/default.nix
@@ -1,30 +1,5 @@
 { pkgs, lib, ... }:
 let
-  /*
-    cfg = cfg = config.nix-polyglot;
-    editors = [
-    lib.optionalString cfg.vscode.enable "vscode"
-    lib.optionalString cfg.neovim.enable "neovim"
-    ];
-    editorFilter = key: value:
-    if (editors != []) then (value == "directory" && (elem key editors || key == "default") || value == "file" && (elem (removeSuffix ".nix" key) editors))
-    else [];
-    folder = ./.;
-    toImportLang = name: value: folder + ("/" + name);
-    toImportEditor = lang: name: value: folder + ("/" + name);
-
-    filterCaches = key: value: value == "directory" && (elem key cfg.langs || elem "all" cfg.langs || key == "default");
-    langList = lib.mapAttrsToList toImportLang
-    (lib.filterAttrs filterCaches (builtins.readDir folder));
-
-    getImports = lang: lib.mapAttrsToList toImportEditor lang
-    (lib.filterAttrs editorFilter (builtins.readDir (toImportLang langList)));
-    optional cfg.${editor}.enable ++ (lib.optionalString [isPath / + "${lang}/default.nix"] lang)
-
-    imports = [./default] ++ (flatten (forEach langList (lang: lib.mapAttrsToList toImportEditor lang
-    (lib.filterAttrs editorFilter (builtins.readDir (toImportLang langList)));
-    optional cfg.${editor}.enable ++ (lib.optionalString [isPath / + "${lang}/default.nix"] lang)) );
-  */
   folder = ./.;
   toImport = name: value: folder + ("/" + name);
   filterCaches = key: value: value == "directory";
@@ -32,11 +7,3 @@ let
     (lib.filterAttrs filterCaches (builtins.readDir folder));
 in
 { inherit imports; }
-
-/*
-  New Folder structure
-  lang/${programmingLanguage}/${editor}
-  lang/${programmingLanguage}/${editor}.nix
-
-  ${editor} settings for a ${programmingLanguage} and/or settings for plugins/extensions
-*/
