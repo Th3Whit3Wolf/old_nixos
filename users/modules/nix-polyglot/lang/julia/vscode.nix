c4c476b7931e4028b6271019871646f40f879f97
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2021-07-19 22:16:03 +0100

    [Nix-Polyglot] Added julia support

---
+++ users/modules/nix-polyglot/lang/julia/vscode.nix
@@ -0,0 +1,44 @@
+{ config, options, lib, pkgs, ... }:
+with lib;
+with builtins;
+let
+  currLang = baseNameOf (builtins.toString ./.);
+  polyglot = config.nix-polyglot;
+  cfg = polyglot.lang.${currLang};
+  enabled = (elem currLang polyglot.langs || elem "all" polyglot.langs) && polyglot.vscode.enable;
+  jsonFormat = pkgs.formats.json { };
+
+  langExtensions = with pkgs.vscode-extensions; [
+    julialang.language-julia
+  ];
+
+in
+{
+  options.nix-polyglot.lang.${currLang}.vscode = {
+    extensions = mkOption {
+      type = types.listOf types.package;
+      default = langExtensions;
+      example = literalExample "[ pkgs.vscode-extensions.matklad.rust-analyzer ]";
+      description = ''
+        The extensions Visual Studio Code should use with for programming in ${currLang}.
+        These will override but not delete manually installed ones.
+      '';
+    };
+    settings = mkOption {
+      type = jsonFormat.type;
+      example = literalExample ''
+        langSettings = {
+          rust-analyzer = {
+            cargo.allFeatures = true;
+            checkOnSave.command = "clippy";
+            procMacro.enable = true;
+          };
+        };
+      '';
+      default = (toJSON { });
+      description = ''
+        User settings for vscode related to ${currLang}.
+      '';
+    };
+  };
+}
