015a61c7e9628cf74f953f897b9d5a2faa547d7f
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2021-07-20 17:13:21 +0100

    Add SFMono-nerfonts

---
+++ pkgs/fonts/sanFranciscoMono-font/default.nix
@@ -0,0 +1,21 @@
+{ lib, stdenv, source }:
+
+stdenv.mkDerivation rec {
+  inherit (source) src pname version;
+  buildPhase = "true";
+
+  installPhase = ''
+    mkdir -p $out/share/fonts/san-francisco-mono
+    cp *.otf $out/share/fonts/san-francisco-mono
+  '';
+
+  meta = with lib; {
+    description = "San Francisco Mono Font by Apple";
+    homepage = "https://github.com/supercomputra/SF-Mono-Font";
+
+    maintainers = [ maintainers.Th3Whit3Wolf ];
+    platforms = platforms.unix;
+    license = licenses.mit;
+    inherit version;
+  };
+}
