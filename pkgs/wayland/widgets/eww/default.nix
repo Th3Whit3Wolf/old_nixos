dbe3c0b78e42f838eff27b4e7ddf3f7b2703cfdf
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2021-07-26 08:23:18 +0100

    add pkgs eww

--- pkgs/wayland/widgets/eww/default.nix
+++ pkgs/wayland/widgets/eww/default.nix
@@ -1,1 +1,49 @@
+{ lib, pkgs, source, ... }:
 
+let
+  mkRustPlatform = pkgs.callPackage ./mk-rust-platform.nix { };
+  rustPlatform = mkRustPlatform {
+    date = "2021-07-15";
+    channel = "nightly";
+  };
+in
+
+rustPlatform.buildRustPackage rec {
+  inherit (source) pname version src;
+
+  cargoPatches = [
+    # a patch file to add/update Cargo.lock in the source code
+    ./update-Cargo.lock.patch
+  ];
+  cargoSha256 = "sha256-D3yU10Rz4vTTJbHjuYyCi/Kt1ZEMB32J93h6i38OB4w=";
+  cargoBuildFlags = [ "--no-default-features" "--features=wayland" ];
+  nativeBuildInputs = with pkgs; [
+    pkgs.wrapGAppsHook
+    pkg-config
+  ];
+
+  buildInputs = with pkgs; [
+    gtk3
+    cairo
+    glib
+    glib.dev
+    atk
+    pango
+    gdk-pixbuf
+    gdk-pixbuf-xlib
+    wayland
+    wayland-protocols
+    gtk-layer-shell
+    gtk-layer-shell.dev
+  ];
+
+  doCheck = false;
+  checkPhase = null;
+
+  meta = with lib; {
+    description = "A standalone widget system made in Rust to add AwesomeWM like widgets to any WM";
+    homepage = "https://github.com/elkowar/eww";
+    license = licenses.mit;
+    maintainers = with maintainers; [ fortuneteller2k ];
+  };
+}
