e138ee73347051ffd992a52e2d528e7f513eba4b
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2021-08-02 09:51:04 +0100

    Change how eww profile is done

--- users/profiles/desktop/widgets/eww/default.nix
+++ users/profiles/desktop/widgets/eww/default.nix
@@ -1,50 +1,62 @@
 { config, lib, pkgs, ... }:
 
+let
+  defs = lib.forEach
+    (
+      lib.mapAttrsToList (n: v: lib.removeSuffix ".nix" "${n}") (builtins.readDir ./definitions)
+    )
+    (x:
+      ''<def name="${x}">
+    ${(import (./definitions + "/${x}.nix") { lib = lib; pkgs = pkgs; }).def}
+    </def>''
+    );
+in
 {
+  /*
+    config = {
+    xdg.configFile = {
+    "eww/eww.scss" = {
+    source = ./eww.scss;
+    };
+    "eww/eww.xml" = {
+    source =./eww.xml;
+    };
+    };
+    };
+  */
+
   config.services.eww = {
     enable = true;
     isWayland = true;
+    style = ./eww.scss;
     config = {
-      definitions = {
-        clock = {
-          box = {
-            contents = "The time is: {{my_time}} currently.";
-          };
-        };
-        main = {
-          box = {
-            content = {
-              name = "clock";
-              args = {
-                my_time = "date";
-              };
-            };
-          };
-        };
-      };
+      definitions = defs;
       variables = {
         script-vars = {
           date = { text = "date"; };
+          networking = {
+            text = "nmcli -p -g {connection} | grep -e 'connected to' | sed -e 's/ .* //' -e 's/.*://'";
+            interval = {
+              quantity = 60;
+              units = "s";
+            };
+          };
         };
         vars = { };
       };
       windows = {
-        main = {
-          stacking = "fg";
-          focusable = "false";
-          screen = 1;
+        bar = {
+          stacking = "bt";
+          focusable = false;
+          exclusive = true;
           geometry = {
-            anchor = "top left";
-            x = "300px";
-            y = "50%";
-            width = "25%";
-            height = "20px";
+            anchor = "top center";
+            x = "0px";
+            y = "0px";
+            width = "100%";
+            height = "12px";
           };
-          reserve = {
-            side = "left";
-            distance = "50px";
-          };
-          widget = "main";
+          widget = "bar";
         };
       };
     };
