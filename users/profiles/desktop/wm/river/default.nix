81dab3d9819d50b187e908640122ce7fa3da4baf
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2021-08-03 11:26:03 +0100

    Update river

--- users/profiles/desktop/wm/river/default.nix
+++ users/profiles/desktop/wm/river/default.nix
@@ -1,25 +1,20 @@
-{ config, lib, pkgs, ... }:
-
-let
-  inherit (config.home) homeDirectory username;
-  pactl = "${pkgs.pulseaudioFull}/bin/pactl";
-  brightnessctl = "${pkgs.brightnessctl}/bin/brightnessctl";
-  lockCommand =
-    "${pkgs.swaylock-effects}/bin/swaylock --screenshots --clock --indicator --indicator-radius 100 --indicator-thickness 7 --effect-blur 7x5 --effect-vignette 0.5:0.5 --effect-pixelate 3 --ring-color 5d4d7a --grace 2 --fade-in 0.7";
-in
-{
-  home.configFile = {
-    "river/init" = {
-      text = ''
-        #!/bin/sh
-        mod="Mod4"
-        riverctl map normal $mod Return spawn alacritty
-        riverctl map normal $mod W spawn firefox
-        # Mod+Q to close the focused view
-        riverctl map normal $mod Q close
-        # Mod+E to exit river
-        riverctl map normal $mod E exit
-      '';
-    };
-  };
+{ pkgs, ... }: {
+  xdg.configFile.".river/init".source = ./init;
+  xdg.configFile.".river/layout".source = ./layout;
+  home.packages = [
+    alacritty
+    #brightnessctl
+    wofi
+    kanshi
+    swaylock-effects
+    swayidle
+    grim
+    sway-contrib.grimshot
+    imv
+    slurp
+    qt5.qtwayland
+    nwg-launchers
+    wl-clipboard
+    mpv
+  ];
 }
