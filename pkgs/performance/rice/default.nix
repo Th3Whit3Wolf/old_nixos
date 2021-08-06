107e2701f20ea019576e6f9ca5c37939edb7becb
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2021-08-06 17:34:14 +0100

    Rice now built with musl

--- pkgs/performance/rice/default.nix
+++ pkgs/performance/rice/default.nix
@@ -1,12 +1,10 @@
-{ lib, pkgs, source, rustPlatform, rust-bin, fetchFromGitHub, ... }:
-
+{ rustPlatform, fetchFromGitHub, llvmPackages_latest, source, pkgsMusl, rust, stdenv, linuxHeaders, buildPackages, rust-bin }:
 let
-  #rustSpecific = rust-bin.stable.latest.minimal.override {
-  #    targets = [ "x86_64-unknown-linux-musl" ];
-  #};
-  #rustc = rustSpecific;
-  #cargo = rustSpecific;
-  #rustPlatform = makeRustPlatform { inherit cargo rustc; };
+  hostTarget = rust.toRustTargetSpec stdenv.hostPlatform;
+  muslTarget = rust.toRustTargetSpec pkgsMusl.stdenv.hostPlatform;
+  rustSpecific = rust-bin.stable.latest.minimal.override {
+    targets = [ "x86_64-unknown-linux-musl" ];
+  };
   Ananicy = fetchFromGitHub {
     repo = "Ananicy";
     owner = "Nefelim4ag";
@@ -14,9 +12,6 @@ let
     sha256 = "sha256-++vxAYMCUuvNRYbyWabkfy++io2h/R7mwInFjMwNRro=";
   };
 in
-
-
-
 rustPlatform.buildRustPackage rec {
   inherit (source) pname version src;
 
@@ -25,35 +20,33 @@ rustPlatform.buildRustPackage rec {
     ./modify-Cargo.toml.patch
     ./modify-src.patch
   ];
-
-  cargoSha256 = "sha256-ehqdar57S0KOneTX3SWvoPorI+xeL4zBLKiPrz7lPH0=";
-  cargoBuildFlags = [ "--no-default-features" "--features=syslog" ];
-  #CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
-  #CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_LINKER = "${[kgs].llvmPackages_10.lld}/bin/lld";
-
-  #target = "x86_64-unknown-linux-musl";
-  nativeBuildInputs = with pkgs; [
-    pkg-config
+  nativeBuildInputs = [
+    buildPackages.pkg-config
+    rustSpecific
+  ];
+  buildInputs = [
+    linuxHeaders
   ];
 
-  buildInputs = with pkgs; [
-    pkg-config
-  ];
+  # There are no tests to run
+  doCheck = false;
+  CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_LINKER = "${llvmPackages_latest.lld}/bin/lld";
+  LIBCLANG_PATH = "${llvmPackages_latest.libclang.lib}/lib";
+
+  buildPhase = ''
+    runHook preBuild
+    export BINDGEN_EXTRA_CLANG_ARGS=$NIX_CFLAGS_COMPILE
+    export RUSTFLAGS="-C target-feature=+crt-static -C link-arg=-s"
+    cargo build --frozen --release --target=${muslTarget} --target-dir=target --no-default-features --features=syslog
+    mv target/${muslTarget}/ target/${hostTarget}
+    runHook postBuild
+  '';
 
   postInstall = ''
     mkdir -p $out/{bin,rules}
-    mv target/x86_64-unknown-linux-gnu/release/rice-bin $out/bin/rice
+    mv $out/bin/rice-bin $out/bin/rice
     cp -r ${Ananicy}/ananicy.d/* $out/rules/
-    strip --strip-all $out/bin/rice
     chmod +x $out/bin/rice
   '';
-
-  doCheck = false;
-  checkPhase = null;
-
-  meta = with lib; {
-    description = "A standalone widget system made in Rust to add AwesomeWM like widgets to any WM";
-    homepage = "https://github.com/elkowar/eww";
-    license = licenses.mit;
-  };
+  cargoSha256 = "sha256-ehqdar57S0KOneTX3SWvoPorI+xeL4zBLKiPrz7lPH0=";
 }
