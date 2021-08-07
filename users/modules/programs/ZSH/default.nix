637099467f220af6279c3dee9ddacea4dc813f6d
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2021-08-08 00:09:07 +0100

    Fix potential issue in zsh

--- users/modules/programs/ZSH/default.nix
+++ users/modules/programs/ZSH/default.nix
@@ -622,6 +622,11 @@ in
         typeset -g -A key
         typeset -U path cdpath fpath manpath
 
+        export XDG_CONFIG_HOME=''${XDG_CONFIG_HOME:=''${HOME}/.config}
+        export XDG_CACHE_HOME=''${XDG_CACHE_HOME:=''${HOME}/.cache}
+        export XDG_DATA_HOME=''${XDG_DATA_HOME:=''${HOME}/.local/share}
+        export XDG_BIN_HOME=''${XDG_BIN_HOME:=''${HOME}/.local/bin}
+
         ${optionalString (cfg.cdpath != [ ]) ''
           cdpath+=(${concatStringsSep " " cfg.cdpath})
         ''}
