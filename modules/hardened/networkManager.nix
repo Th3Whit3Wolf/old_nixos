1773f1dba7f6f3e8c9f7d276f14763074a008369
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2021-08-08 00:09:40 +0100

    Stricter NetworkManager

--- modules/hardened/networkManager.nix
+++ modules/hardened/networkManager.nix
@@ -4,39 +4,165 @@
   config = lib.mkIf (config.networking.networkmanager.enable) {
     systemd.services = {
       NetworkManager.serviceConfig = {
-        # Used as root directory
-        #RuntimeDirectory = "NetworkManager";
-        #RootDirectory = "/run/NetworkManager";
-        RestrictAddressFamilies = [ "AF_INET" "AF_INET6" "AF_NETLINK" "AF_PACKET" "AF_UNIX" ];
-        ProtectHome = true;
-        #ProtectSystem = "strict";
-        #ProtectProc = "invisible";
-        #ReadWritePaths = ["/etc" "/proc/sys/net" "/run/resolvconf" "/var/lib/NetworkManager" "/var/run/NetworkManager"  ];
-        PrivateTmp = true;
-        #PrivateDevices = true;
-        #ProtectKernelTunables = true;
-        #ProtectKernelModules = true;
-        #ProtectKernelLogs = true;
-        UMask = 077;
-        #CapabilityBoundingSet = ["~CAP_SYS_ADMIN" "CAP_SETUID" "CAP_SETGID" "CAP_SYS_CHROOT"];
-        NoNewPrivileges = true;
-        #ProtectHostname = true;
-        ProtectClock = true;
-        ProtectControlGroups = true;
-        #RestrictNamespaces = true;
-        #LockPersonality = true;
-        #MemoryDenyWriteExecute = true;
-        RestrictRealtime = true;
-        RestrictSUIDSGID = true;
-        #SystemCallFilter = ["@system-service" "~@resources" "@privileged"];
-        #SystemCallArchitectures = "native";
-        BindReadOnlyPaths = [
-          "/nix/store"
-          "/etc/NetworkManager"
-        ];
+        /*
+          * Networking
+        */
+
+        # PrivateNetwork= service needs access to host network
+        RestrictAddressFamilies = "AF_INET AF_INET6 AF_NETLINK AF_PACKET AF_UNIX";
+        # IPAccounting = "yes";
+        # IPAddressAllow = "any";
+        # IPAddressDeny= service needs access to all IPs
+
+        /*
+          * File system
+        */
+
+        #  Note that the effect of these settings may be undone by privileged processes. In order to
+        #  set up an effective sandboxed environment for a unit it is thus recommended to combine
+        #  these settings with either CapabilityBoundingSet=~CAP_SYS_ADMIN or
+        #  SystemCallFilter=~@mount.
+
+        ProtectHome = "yes";
+        ProtectSystem = "full";
+        ProtectProc = "ptraceable";
+        ReadWritePaths = "/etc/resolv.conf /etc/machine-id -/etc/NetworkManager -/proc/sys/net -/run/resolvconf -/var/lib/NetworkManager -/var/run/NetworkManager";
+        PrivateTmp = "yes";
+
+        /*
+          * User separation
+        */
+
+        # PrivateUsers= service runs as root
+        # DynamicUser= service runs as root
+
+        /*
+          * Devices
+        */
+
+        PrivateDevices = "yes";
+        # DeviceAllow=/dev/exampledevice
+
+        /*
+          * Kernel
+        */
+
+        ProtectKernelTunables = "yes";
+        ProtectKernelModules = "yes";
+        ProtectKernelLogs = "yes";
+
+        /*
+          * Misc
+        */
+
+        UMask = "077";
+        CapabilityBoundingSet = "~CAP_SYS_ADMIN CAP_SETUID CAP_SETGID CAP_SYS_CHROOT";
+        # AmbientCapabilities= service runs as root
+        NoNewPrivileges = "yes";
+        ProtectHostname = "yes";
+        ProtectClock = "yes";
+        ProtectControlGroups = "yes";
+        RestrictNamespaces = "yes";
+        LockPersonality = "yes";
+        MemoryDenyWriteExecute = "yes";
+        RestrictRealtime = "yes";
+        RestrictSUIDSGID = "yes";
+        # RemoveIPC= service runs as root
+
+        /*
+          * System calls
+        */
+
+        #SystemCallFilter = [
+        #  "@system-service"
+        #  "~@resources @privileged"
+        #];
+
+        SystemCallArchitectures = "native";
+
       };
       NetworkManager-dispatcher.serviceConfig = {
-        ProtectHome = true;
+        /*
+          * Networking
+        */
+
+        # PrivateNetwork= service needs access to host network
+        #RestrictAddressFamilies = "AF_INET AF_INET6 AF_NETLINK AF_PACKET AF_UNIX";
+        # IPAccounting = "yes";
+        # IPAddressAllow = "any";
+        # IPAddressDeny= service needs access to all IPs
+
+        /*
+          * File system
+        */
+
+        #  Note that the effect of these settings may be undone by privileged processes. In order to
+        #  set up an effective sandboxed environment for a unit it is thus recommended to combine
+        #  these settings with either CapabilityBoundingSet=~CAP_SYS_ADMIN or
+        #  SystemCallFilter=~@mount.
+
+        ProtectHome = "yes";
+        ProtectSystem = "full";
+        ProtectProc = "ptraceable";
+        ReadWritePaths = "-/etc/NetworkManager/dispatcher.d";
+        PrivateTmp = "yes";
+
+        #BindReadOnlyPaths = [
+        #  "/nix/store"
+        #];
+
+        /*
+          * User separation
+        */
+
+        # PrivateUsers= service runs as root
+        # DynamicUser= service runs as root
+
+        /*
+          * Devices
+        */
+
+        PrivateDevices = "yes";
+        # DeviceAllow=/dev/exampledevice
+
+        /*
+          * Kernel
+        */
+
+        ProtectKernelTunables = "yes";
+        ProtectKernelModules = "yes";
+        ProtectKernelLogs = "yes";
+
+        /*
+          * Misc
+        */
+
+        UMask = "077";
+        CapabilityBoundingSet = "~CAP_SYS_ADMIN CAP_SETUID CAP_SETGID CAP_SYS_CHROOT";
+        # AmbientCapabilities= service runs as root
+        NoNewPrivileges = "yes";
+        ProtectHostname = "yes";
+        #ProtectClock = "yes";
+        ProtectControlGroups = "yes";
+        RestrictNamespaces = "yes";
+        #LockPersonality = "yes";
+        MemoryDenyWriteExecute = "yes";
+        RestrictRealtime = "yes";
+        RestrictSUIDSGID = "yes";
+        RemainAfterExit = "yes";
+        # RemoveIPC= service runs as root
+
+        /*
+          * System calls
+        */
+
+        #SystemCallFilter = [
+        #  "@system-service"
+        #  "~@resources @privileged"
+        #];
+
+        SystemCallArchitectures = "native";
+
       };
     };
   };
