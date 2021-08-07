2045b58d3d4a012151136aa43e725c049e06810e
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2021-08-07 14:11:49 +0100

    Dont touch logind

--- modules/hardened/default.nix
+++ modules/hardened/default.nix
@@ -2,9 +2,5 @@
   imports = [
     ./networkManager.nix
   ];
-
-  config.systemd.services.systemd-logind.serviceConfig = {
-    SupplementaryGroups = "proc";
-  };
 }
 # https://github.com/krathalan/systemd-sandboxing
