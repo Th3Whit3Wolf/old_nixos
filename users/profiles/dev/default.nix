db48f9228d9e35d7616ebebf0a95e73f3a2b1c31
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2021-09-04 10:37:54 +0100

    Update dev settings

--- users/profiles/dev/default.nix
+++ users/profiles/dev/default.nix
@@ -1,16 +1,28 @@
 { config, lib, pkgs, ... }:
 
+let
+  inherit (config.home) homeDirectory;
+in
 {
-  home.nix-polyglot = {
-    enable = true;
-    langs = [ "all" ];
-    enableZshIntegration = false;
-    neovim.enable = true;
-    vscode = {
+  home = {
+    nix-polyglot = {
       enable = true;
-      package = pkgs.vscodium;
+      langs = [ "all" ];
+      enableZshIntegration = false;
+      neovim.enable = true;
+      vscode = {
+        enable = true;
+        package = pkgs.vscodium;
+      };
+    };
+    sessionVariables = {
+      HELIX_RUNTIME = "${homeDirectory}/.config/helix/runtime";
     };
   };
+  systemd.user.sessionVariables = {
+    HELIX_RUNTIME = "${homeDirectory}/.config/helix/runtime";
+  };
+  services.gpg-agent.pinentryFlavor = "gnome3";
 }
 
 /*
